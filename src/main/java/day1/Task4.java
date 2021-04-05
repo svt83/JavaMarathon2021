68276ffd280f1aff1c3547dfd99c97cc90b5086f
= <=>
2021-04-05 22:32:06 +0700

    First Commit All Project JavaMarathon2021

---
+++ src/main/java/day1/Task4.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int year = 1980;
+        do {
+            System.out.println("Олимпиада " + year + " года");
+            year += 4;
+        } while (year <= 2020);
+    }
+}
