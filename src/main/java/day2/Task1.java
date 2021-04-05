6c51df70221368e6a5fcef71e7afb2b839e06b94
= <=>
2021-04-05 22:58:14 +0700

    Day2Task2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,24 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        int stage;
+        Scanner input = new Scanner(System.in);
 
+        /*System.out.print("Введите номер этажа: ");*/
+        stage = input.nextInt();
+
+        if (stage >= 1 && stage <= 4) {
+            System.out.println("Среднеэтажный дом");
+        }
+        else if (stage >= 5 && stage <= 8) {
+            System.out.println("Среднеэтажный дом");
+        }
+        else if (stage >= 9) {
+            System.out.println("Многоэтажный дом");
+        }
+        else System.out.println("Ошибка ввода");
     }
 }
